8aa3951c3dcf163cf56d8a1933708e3f1ac83f34
marco49 <marco@01lab.com>
2021-10-03 01:37:17 -0500

    [FIX] secure mongo uri

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
   DB_HOST:
-    process.env.NODE_ENV === "dev" ? process.env.DB_HOST : process.env.DB_HOST,
+    process.env.NODE_ENV === "dev"
+      ? process.env.DB_HOST_TEST
+      : process.env.DB_HOST,
 };
