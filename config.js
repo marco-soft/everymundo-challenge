dfc7202babf93b39c62dda0615e1a9874db7c184
marco49 <marco@01lab.com>
2021-10-03 01:35:10 -0500

    [FIX] secure mongo uri

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
   DB_HOST:
-    process.env.NODE_ENV === "dev"
-      ? "mongodb+srv://root:jLOQCcttXynZL4tt@everymundochallenge.pdwhu.mongodb.net/everyMundoTest?retryWrites=true&w=majority"
-      : process.env.DB_HOST,
+    process.env.NODE_ENV === "dev" ? process.env.DB_HOST : process.env.DB_HOST,
 };
